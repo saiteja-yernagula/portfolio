8f35aee6bc38eb69218bc6cf59ef89e73e186514
saiteja-yernagula <saiteja.yernagula@gmail.com>
2023-07-08 16:04:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+import Calculator from './Calculator';
+import Todo from './Todo';
+import Counter from './Counter';
+import Weather from './Weather';
+import Crypto from './Crypto';
+
+import Recipe from './Recipe';
+import Pixabay from './Pixabay';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Search from './Search';
+import Folio from './Folio';
+
+
+
+function App (){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+    
+    <div>
+    <BrowserRouter>
+        
+         <Routes>
+            <Route path='/' element={<Folio/>}/>
+            <Route path='/gallery' element={<Pixabay/>}/>
+            <Route path='/gallery/search/:gdata' element={<Search/>}/>
+            <Route path='/calculator' element={<Calculator/>}/>
+            <Route path='/weather' element={<Weather/>}/>
+            <Route path='/crpyto' element={<Crypto/>}/>
+            <Route path='/recipe' element={<Recipe/>}/>
+            <Route path='/counter' element={<Counter/>}/>
+            <Route path='/todo' element={<Todo/>}/>
+
+
+         </Routes>
+    </BrowserRouter>
     </div>
-  );
+    </>
+    
+  )
 }
 
-export default App;
+export default App
